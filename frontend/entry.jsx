d6080a631e59f721e39ec98e356751d136e8afaa
Marco Arduini <mar.arduini@gmail.com>
2017-01-16 11:08:02 +0100

    Store and maintain state in localStorage

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,21 +4,11 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-const store = configureStore();
-
-// TODO: just for testing!
-import allTodos from './reducers/selectors';
-import { receiveTodo, receiveTodos } from './actions/todo_actions';
-import { receiveSteps, receiveStep, removeStep } from './actions/step_actions';
-window.store = store;
-window.allTodos = allTodos;
-window.receiveTodo = receiveTodo;
-window.receiveTodos = receiveTodos;
-window.receiveStep = receiveStep;
-window.receiveSteps = receiveSteps;
-window.removeStep = removeStep;
-
 document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.getItem('state') ?
+    JSON.parse(localStorage.getItem('state')) : {};
+  const store = configureStore(preloadedState);
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
